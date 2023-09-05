cd81a1e7569f6a21d45610de8531b1a6b14ea8a6
William Cunningham <William.cunningham@live.com>
2023-09-05 15:13:52 -0400

    Add data_name to Activity Model.

--- db/migrate/20230905190015_create_activities.rb
+++ db/migrate/20230905190015_create_activities.rb
@@ -3,6 +3,7 @@ class CreateActivities < ActiveRecord::Migration[7.0]
     create_table :activities do |t|
       t.references :user, null: false, foreign_key: true
       t.string :name
+      t.string :data_name
       t.string :action_type
       t.string :status
       t.boolean :includes_duration
